0c1bcbaacd25a12f4d8093b8caf26a6327f16bc1
jeyog <choijy8816@gmail.com>
2022-08-24 22:53:01 +0900

    add type about

---
+++ src/functions/app/types/about.ts
@@ -0,0 +1,9 @@
+export interface About {
+    id: number | null
+    githubId: number
+    name: string
+    profileUrl: string | null
+    job: string
+    introduction: string
+    instagramUrl: string | null
+}
